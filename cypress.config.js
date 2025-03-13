28c84f78090625ead52680523c1a368e66cc9ca0
The-Killdozer <f_valerio@hotmail.com>
2025-03-13 14:55:36 +0100

    Agregado Mochawesome Resports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 import { defineConfig } from 'cypress';
+import mochawesome from 'cypress-mochawesome-reporter/plugin.js';
+
 export default defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      mochawesome(on);
     },
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'DemoBlaze UI Tests',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: false,
+    html: true,
+    json: true,
+    autoOpen: false,
+    code: true,
+    timestamp: 'longDate',
+    showPassed: true,
+    saveAllAttempts: false,
   },
 });
